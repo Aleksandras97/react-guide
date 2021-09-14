6a976eac4c0e2ee9a0342a46373df63eb8e8c412
aleksandrsa97 <aleksandrnarusevic1@gmail.com>
2021-09-14 15:19:43 +0300

    optimization exmaple

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
+import { lazy, Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
+// import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = lazy(() => import("./pages/NewQuote"));
 
 function App() {
   return (
     <MainLayout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </MainLayout>
   );
 }
