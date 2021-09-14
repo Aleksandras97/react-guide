54a09d049d7c362eee8dd872937e5f0a67db05f1
aleksandrsa97 <aleksandrnarusevic1@gmail.com>
2021-09-15 00:45:09 +0300

    added register and login api calls

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,34 +1,84 @@
-import { useState } from 'react';
+import axios from "axios";
+import { useState, useRef } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const emailInputRef = useRef("");
+  const passwordInputRef = useRef("");
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    let url;
+
+    if (isLogin) {
+      //Login
+      console.log('Login');
+
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCRDMr3MQO9CtQD9OywxW7ef48W9_1jyiw";
+    } else {
+      //Register
+      console.log('Register');
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCRDMr3MQO9CtQD9OywxW7ef48W9_1jyiw";
+    }
+    setIsLoading(true);
+
+    await axios
+      .post(url, {
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      })
+      .then((res) => {
+        const resData = res.data;
+
+        console.log(resData);
+      })
+      .catch(({ response }) => {
+        let errorMessage = "Authentication failed";
+        alert(errorMessage);
+      });
+    setIsLoading(false);
+  };
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      {!isLoading && <h1>{isLogin ? "Login" : "Sign Up"}</h1>}
+      {isLoading && <p>Sending request</p>}
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
